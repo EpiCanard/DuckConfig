c82047804a7fe3daeff4b910b0f467997ffa3388
EpiCanard <epicanard@gmail.com>
2020-04-30 21:08:46 +0200

    Fix wrong classloader and add some javadoc

--- src/main/java/fr/epicanard/duckconfig/DuckLoader.java
+++ src/main/java/fr/epicanard/duckconfig/DuckLoader.java
@@ -11,22 +11,44 @@ import java.util.Map;
 
 public class DuckLoader {
 
+  /**
+   * Load a class from a yaml. Use Resource annotation to find where load yaml
+   *
+   * @param configClass Class to deserialize into
+   * @param <T>         Type of class
+   * @return Return a new instance of class deserialized from yaml
+   */
   public static <T> T load(final Class<T> configClass) {
     return load(configClass, (String) null);
   }
 
+  /**
+   * Load a class from a yaml. Use Resource annotation to find where load yaml
+   *
+   * @param configClass Class to deserialize into
+   * @param basePath    Base path of file to search
+   * @param <T>         Type of class
+   * @return Return a new instance of class deserialized from yaml
+   */
   public static <T> T load(final Class<T> configClass, final String basePath) {
     final Resource resource = getResource(configClass);
     return load(configClass, new ResourceWrapper(basePath, resource));
   }
 
+  /**
+   * Load a class from a yaml
+   *
+   * @param configClass Class to deserialize into
+   * @param wrapper     Resource wrapper that contains load information
+   * @param <T>         Type of class
+   * @return Return a new instance of class deserialized from yaml
+   */
   public static <T> T load(final Class<T> configClass, final ResourceWrapper wrapper) {
-    final InputStream inputStream = loadFile(wrapper);
+    final InputStream inputStream = loadFile(wrapper, configClass.getClassLoader());
     if (inputStream != null) {
       return wrapper.type.parser().load(inputStream, configClass);
     }
     try {
-      System.out.println("STREAM NULL");
       return configClass.newInstance();
     } catch (InstantiationException | IllegalAccessException e) {
       e.printStackTrace();
@@ -34,15 +56,29 @@ public class DuckLoader {
     return null;
   }
 
+  /**
+   * Load a generic map of subclass from yaml
+   *
+   * @param configClass Sub class contained in map
+   * @param wrapper     Resource wrapper that contains load information
+   * @param <T>         Type of subclass
+   * @return Return the map of subclass deserialized from yaml
+   */
   public static <T> Map<String, T> loadMap(final Class<T> configClass, final ResourceWrapper wrapper) {
-    final InputStream inputStream = loadFile(wrapper);
+    final InputStream inputStream = loadFile(wrapper, configClass.getClassLoader());
     if (inputStream != null) {
       return wrapper.type.parser().loadMap(inputStream, configClass);
     }
     return new HashMap<>();
-
   }
 
+  /**
+   * Save the config class to yaml
+   *
+   * @param config  Config class to save
+   * @param wrapper Resource wrapper that contains information to save the file
+   * @param <T>     Type of config
+   */
   public static <T> void save(final T config, final ResourceWrapper wrapper) {
     try {
       final FileOutputStream stream = new FileOutputStream(new File(wrapper.basePath, wrapper.value));
@@ -59,7 +95,13 @@ public class DuckLoader {
    *    TOOLS
    * =========== */
 
-  private static <T> Resource getResource(final Class<T> configClass) {
+  /**
+   * Get the resource annotation of class or throw an exception if missing
+   *
+   * @param configClass Class to analyze
+   * @return Return Resource found or throw a MissingResourceException
+   */
+  private static Resource getResource(final Class<?> configClass) {
     final Resource resource = configClass.getAnnotation(Resource.class);
     if (resource == null) {
       throw new MissingResourceException(configClass.getName());
@@ -68,18 +110,19 @@ public class DuckLoader {
   }
 
   /**
-   * Load the file sent in parameter and res
+   * Load the file sent in parameter in resource wrapper
    *
-   * @param wrapper Resource wrapper that contains all information of resource
+   * @param wrapper     Resource wrapper that contains all information of resource
+   * @param classLoader ClassLoader to use to load inner file
    * @return Return InputStream of file
    */
-  private static InputStream loadFile(final ResourceWrapper wrapper) {
+  private static InputStream loadFile(final ResourceWrapper wrapper, final ClassLoader classLoader) {
     try {
       if (wrapper.location == ResourceLocation.FILE_PATH) {
         return new FileInputStream(new File(wrapper.basePath, wrapper.value));
       } else {
         final String resourcePath = ((wrapper.basePath == null) ? "" : wrapper.basePath + "/") + wrapper.value;
-        return DuckLoader.class.getClassLoader().getResourceAsStream(resourcePath);
+        return classLoader.getResourceAsStream(resourcePath);
       }
     } catch (FileNotFoundException e) {
       e.printStackTrace();
